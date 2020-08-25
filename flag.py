ba0e4fccf33ff57fd1b1209e11b37be705a20ead
Hmrinal <mrinaltyagi1912@gmail.com>
2020-08-25 21:07:25 +0530

    chakra added

--- flag.py
+++ flag.py
@@ -26,5 +26,16 @@ py.plot(6,4, marker='o', markerfacecolor='#000088ff',markersize=9.5)
 chakra = py.Circle((6,4),radius,color='#000088ff',fill=False,linewidth=7)
 n.add_artist(chakra)
 
+for i in range(0,24):
+    p = 6+radius/2 * np.cos(np.pi*i/12 + np.pi/48)
+    q = 6+radius/2 * np.cos(np.pi*i/12 - np.pi/48)
+    r = 4+radius/2 * np.sin(np.pi*i/12 + np.pi/48)
+    s = 4+radius/2 * np.sin(np.pi*i/12 - np.pi/48)
+    t = 6+radius * np.cos(np.pi*i/12)
+    u = 4+radius * np.sin(np.pi*i/12)
+    n.add_patch(patch.Polygon([[6,4],[p,r],[t,u],[q,s]], fill=True,closed=True,color='#000088ff'))
+
+py.axis('equal')
+
 
 py.show()
